724ea67c86272bf7043a6865d824777ef5564b26
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-05-17 19:51:19 +0700

    Perbaikan tampilan layout, Penambahan Navbar

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -11,20 +11,30 @@
             form ul { margin-left: 0; list-style: none; }
             .error { color: red; font-style: italic; }
             body { padding-top: 20px; }
+            .content { padding-top: 3em; }
+            #navbar-1 { padding-right: 1em; }
         </style>
         <title>Daeng PetStore&#0153;</title>
     </head>
 
     <body>
 
-        <div class="container">
-            @if (Session::has('message'))
-                <div class="flash alert">
-                    <p>{{ Session::get('message') }}</p>
-                </div>
-            @endif
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    @include('layouts.navbar')
 
-            @yield('main')
+                    <div class="col-lg-12 content">
+                        @if (Session::has('message'))
+                            <div class="flash alert">
+                                <p>{{ Session::get('message') }}</p>
+                            </div>
+                        @endif
+
+                        @yield('main')
+                    </div>
+                </div>
+            </div>
         </div>
 
     </body>
