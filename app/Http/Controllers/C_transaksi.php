8dbaaead65a7f1d4dec394704e37e293d5666cf6
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-05-23 23:40:20 +0700

    Penambahan fungsi input pembeli beserta form nya pada controller transaksi

--- app/Http/Controllers/C_transaksi.php
+++ app/Http/Controllers/C_transaksi.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Transaksi as Transaksi;
+use App\Pembeli as Pembeli;
 use View, Input, Validator, Redirect;
 
 class C_transaksi extends Controller
@@ -16,5 +17,35 @@ class C_transaksi extends Controller
     	$transaksis->toArray();
 
     	return View::make('transaksi.index', compact('transaksis'));
+    }
+
+    public function create()
+    {
+    	return View::make('transaksi.input');
+    }
+
+    public function store()
+    {
+    	$input = Input::all();
+
+        $insert = Pembeli::create($input);
+
+        if($insert)
+            return Redirect::route('pembeli.index');
+        else
+        {
+            return Redirect::route('transaksi.create')
+                ->with('message', 'Error when insert data.');
+        }
+    }
+
+    // Untuk menginputkan barang yang akan dibeli
+    public function pembelian()
+    {
+    	dd("Sukses");
+    }
+    public function show()
+    {
+    	dd("SUCCESS");
     }
 }
