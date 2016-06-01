dede4ec9a56d7e5165a190f930d57035c0e962b3
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-06-01 23:13:31 +0700

    Penambahan daftar fungsi helper

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -23,3 +23,58 @@ function STOCK($value)
 
 	return $format;
 }
+
+function error_delimiter($type, $message)
+{
+	$close_sign = '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>';
+	switch($type) {
+		case 'warning':
+			$result = '<div class="alert alert-warning">'.$close_sign.$message.'</div>';
+			break;
+		case 'danger':
+			$result = '<div class="alert alert-danger">'.$close_sign.$message.'</div>';
+			break;
+		case 'info':
+			$result = '<div class="alert alert-info">'.$close_sign.$message.'</div>';
+			break;
+		case 'success':
+			$result = '<div class="alert alert-success">'.$close_sign.$message.'</div>';
+			break;
+	}
+
+	return $result;
+}
+
+function dropdown_jbar()
+{
+	$list = array(
+        'Hewan' => 'Hewan',
+        'Makanan' => 'Makanan',
+        'Perlengkapan' => 'Perlengkapan'
+	    );
+
+	return $list;
+}
+
+function dropdown_satuan()
+{
+	$list = array(
+	    'Gram' => 'Gram', 
+	    'Kg' => 'Kg', 
+	    'Pcs' => 'Pcs', 
+	    'Lsn' => 'Lsn', 
+	    'Ekor' => 'Ekor'
+		);
+
+	return $list;
+}
+
+function FormatDateDB($date = null)
+{
+	if(!$date)
+		$date = 'now';
+
+	$newDate = date('Y-m-d H:i:s', strtotime($date));
+
+	return $newDate;
+}
