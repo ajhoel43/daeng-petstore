af8ee776ce2dfbdaf92c4bc4efb9657e3ad96616
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2016-06-09 16:28:00 +0700

    edit tampilan dan penambahan script jquery

--- resources/views/transaksi/input.blade.php
+++ resources/views/transaksi/input.blade.php
@@ -7,11 +7,11 @@
     @if (Session::has('message'))
         {!! Session::get('message') !!}
     @endif
-    {{ Form::open(array('route' => 'transaksi.store', 'class' => 'well')) }}
+    {{ Form::open(array('route' => 'transaksi.store')) }}
         {{ Form::hidden('id', '', array('class' => 'form-control')) }}
         <div class="form-group">
             {{ Form::label('nama', 'Nama Pembeli:', array('class' => 'control-label')) }}
-            {{ Form::text('nama', '', array('class' => 'form-control')) }}
+            {{ Form::text('nama', '', array('class' => 'form-control pembeli_auto')) }}
         </div>
         <div class="form-group">
             {{ Form::label('alamat', 'Alamat:', array('class' => 'control-label')) }}
@@ -20,4 +20,18 @@
         {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
     {{ Form::close() }}
 </div>
+<script>
+$.ajaxSetup({cache:false, async: false});
+
+setTimeout(function(){
+    $(".pembeli_auto").autocomplete({
+        source: "{!! route('autocomplete_pembeli') !!}",
+        select: function(event, ui){
+            $(".pembeli_auto").val(ui.item.value);
+            $("[name='id']").val(ui.item.id);
+            $("[name='alamat']").val(ui.item.alamat);
+        }
+    });
+}, 1500);
+</script>
 @stop
